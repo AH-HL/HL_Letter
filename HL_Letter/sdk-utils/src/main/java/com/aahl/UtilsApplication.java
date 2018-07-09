456b56511e6af51de6ed5321221929f24483b0b6
HLL <anhuihaoliang01@163.com>
2018-07-09 23:50:03 +0800

    组件化初步搭建，后期继续完善ARouter等

---
+++ HL_Letter/sdk-utils/src/main/java/com/aahl/UtilsApplication.java
@@ -0,0 +1,19 @@
+package com.aahl;
+
+/**
+ * @author : Mr.Hao
+ * @date :  2018/4/27
+ * @description :
+ */
+
+public class UtilsApplication extends GlobalApplication {
+
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+
+}
